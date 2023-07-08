2e32d12e9d2dc9daf7fb6aafc05ac7d3b7f8bbe3
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-08 10:12:35 +0530

    signin done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
@@ -9,11 +10,26 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({session}){},
+    async session({session}){
+        const sessionUser = await User.findOne({email:session.user.email});
+        session.user.id = sessionUser._id.toString();
+        return session;
+    },
     async signIn({profile}){
         try {
             await connectToDB();
-            
+
+            //checking if the user already exists
+            const userExists = await User.findOne({email:profile.email});
+
+            //if the user doesnot exists, create a new one
+            if(!userExists){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase,
+                    image:profile.picture
+                });
+            }
 
         } catch (error) {
             console.log(error);
