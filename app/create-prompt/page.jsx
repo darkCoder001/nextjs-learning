b042a94297dc2f728f79303c18b02de169467943
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-11 13:17:56 +0530

    createPrompt function done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -10,7 +10,28 @@ const CreatePrompt = () => {
     const [submitting, setSubmitting]= useState(false);
     const [post, setPost]= useState({ prompt:"", tag:""});
 
-    const CreatePrompt = async (e)=> {};
+    const CreatePrompt = async (e)=> {
+      e.preventDefault();
+      setSubmitting(true);
+      try {
+        const res=await fetch('/api/prompt/new', {
+          method:'POST',
+          body:JSON.stringify({
+            prompt:post.prompt,
+            userId:session?.user.id,
+            tag:post.tag
+          })
+        })
+        if(res.ok){
+          Router.push('/');
+        }
+      } catch (error) {
+        console.log(error);
+      } finally{
+        setSubmitting(false);
+      }
+
+    };
   return (
     <Form
         type="Create"
