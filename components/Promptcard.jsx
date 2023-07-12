dd9328b0da22efab3e2e0df8211f979215e13269
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-12 22:09:03 +0530

    Edit and delete button added

--- components/Promptcard.jsx
+++ components/Promptcard.jsx
@@ -5,6 +5,10 @@ import { useSession } from 'next-auth/react';
 import { useRouter, usePathname } from 'next/navigation';
 
 const Promptcard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+  const { data: session} = useSession();
+  const pathName=usePathname();
+  const router = useRouter();
+
   const [copy, setcopy] = useState("");
   const handleCopy=() => {
     setcopy(post.prompt);
@@ -45,6 +49,22 @@ const Promptcard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>
           {post.tag}
         </p>
+        {session?.user.id === post.creator._id && pathName==='/profile' && (
+          <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+            <p 
+              className='font-inter text-sm green_gradient cursor-pointer'
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p 
+              className='font-inter text-sm orange_gradient cursor-pointer'
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
     </div>
   )
 }
